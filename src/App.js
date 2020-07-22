4de5672088bc2cb66f77ff4179a6da95aa28c47c
Spencer Miller <wspencer.miller@gmail.com>
2020-07-22 18:51:09 -0500

    adding file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-const logo_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSmFLh-OJvCrZmvgXIN41zPRqtF6vTqXcRgcw&usqp=CAU';
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <img src={logo_url} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
           <br/>
           <p>The Anvil</p>
       </header>
